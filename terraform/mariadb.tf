db84cf4e543cd3866b58c994e8f2c5015ec37595
IlanTheUser <ilstore999@gmail.com>
2024-09-08 19:58:50 +0300

    test2

--- terraform/mariadb.tf
+++ terraform/mariadb.tf
@@ -30,14 +30,14 @@ resource "aws_security_group" "db" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    security_groups = [aws_security_group.main.id]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
     ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    security_groups = [aws_security_group.main.id]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
