3cfe0092c7cdfba48cd3146fbcbdf085801ae491
IlanTheUser <ilstore999@gmail.com>
2024-09-11 02:15:13 +0300

    test3

--- terraform/main.tf
+++ terraform/main.tf
@@ -265,6 +265,6 @@ resource "aws_autoscaling_policy" "main" {
     predefined_metric_specification {
       predefined_metric_type = "ASGAverageCPUUtilization"
     }
-    target_value = 70.0
+    target_value = 50.0
   }
 }
